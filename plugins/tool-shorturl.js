249d8442fd7565f4f368b23aef7b149bb896c67b
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-11-29 11:26:35 -0800

    Update tool-shorturl.js

--- plugins/tool-shorturl.js
+++ plugins/tool-shorturl.js
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, text }) => {
-  if (!text) throw '*Please provide a URL or link to shorten.*';
+  if (!text) throw '*Hey Jawn Please provide a URL or link to shorten.*';
 
   let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
   
-  if (!shortUrl1) throw `*Error: Could not generate a short URL.*`;
+  if (!shortUrl1) throw `*Error:Jawn Could not generate a short URL.*`;
 
-  let done = `*SHORT URL CREATED!!*\n\n*Original Link:*\n${text}\n*Shortened URL:*\n${shortUrl1}`.trim();
+  let done = `*SHORT URL CREATED BY UD-MD-BOT!!*\n\n*Original Link:*\n${text}\n*Shortened URL:*\n${shortUrl1}`.trim();
   
   m.reply(done);
 };
