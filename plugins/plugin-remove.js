e69d31ca8b0c731953b65914a08616a24f8f4d0c
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-11-30 07:02:49 -0800

    Update plugin-remove.js

--- plugins/plugin-remove.js
+++ plugins/plugin-remove.js
@@ -15,16 +15,16 @@ const handler = async (m, { conn, usedPrefix: _p, __dirname, args, text }) => {
   const ar = Object.keys(plugins);
   const ar1 = ar.map((v) => v.replace('.js', ''));
 
-  if (!text) throw `📌 *_Example usage:_*\n*#deleteplugin main-menu*`;
+  if (!text) throw `🤦‍♂️ *_Hey Idiot Example usage:_*\n*#deleteplugin main-menu*`;
 
   if (!ar1.includes(args[0])) {
-    return m.reply(`*🗃️ This plugin doesn't exist!*` +
+    return m.reply(`*🗃️ Sorry Jawn This plugin doesn't exist!*` +
       `\n•••••••••••••••••••••••••••••••••••••••••••••••••••••••\n\n${ar1.map((v) => ' ' + v).join(`\n`)}`);
   }
 
   const file = join(__dirname, '../plugins/' + args[0] + '.js');
   unlinkSync(file);
-  conn.reply(m.chat, `⚠️ *_The "plugins/${args[0]}.js" plugin has been deleted._*`, m);
+  conn.reply(m.chat, `⚠️ *_The "plugins/${args[0]}.js" plugin has been deleted by UD-MD._*`, m);
 };
 
 handler.help = ['deleteplugin <name>'];
