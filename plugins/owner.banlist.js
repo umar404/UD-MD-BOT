c2e68e51b242f01a347cc5312bc8d01f66fb6ee5
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-11-30 08:22:51 -0800

    Update owner.banlist.js

--- plugins/owner.banlist.js
+++ plugins/owner.banlist.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     
     m.reply(`
-≡ *USERS BANNED*
+≡ *USERS BANNED BY UD MD BOT*
 
 ▢ Total : *${users.length}* 
 
