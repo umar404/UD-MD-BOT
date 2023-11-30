99cc895125331bff37d1e2b8e02426441816a9fb
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-11-30 08:59:43 -0800

    Update owner-resetprefix.js

--- plugins/owner-resetprefix.js
+++ plugins/owner-resetprefix.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
 
     global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-      await m.reply(`PREFIX RESET SUCCESS`)
+      await m.reply(`👻 JAWN PREFIX RESET SUCCESS`)
   }
   handler.help = ['resetprefix']
   handler.tags = ['owner']
