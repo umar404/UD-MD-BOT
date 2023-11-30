6228d7bed3657a17a6734583f2b0ca33811ec30f
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-11-30 08:53:33 -0800

    Update owner-unbanUser.js

--- plugins/owner-unbanUser.js
+++ plugins/owner-unbanUser.js
@@ -5,11 +5,11 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `✳️ Tag or mention the user to unban`
+    if (!who) throw `😊 Assalamualikum Jawn Tag or mention the user to unban`
     let users = global.db.data.users
     users[who].banned = false
     conn.reply(m.chat, `
-✅ UNBAN
+🤡 UNBAN BY UD-MD-BOT
 
 ───────────
 @${who.split`@`[0]} has been unbanned`, m, { mentions: [who] })
