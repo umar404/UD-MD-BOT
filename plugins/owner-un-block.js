b41a4d65f312f1198b763b734628cd4aafe290f9
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-11-30 08:54:19 -0800

    Update owner-un-block.js

--- plugins/owner-un-block.js
+++ plugins/owner-un-block.js
@@ -1,5 +1,5 @@
 let handler = async (m, { text, conn, usedPrefix, command }) => {
-    let why = `*ERROR, EXAMPLE:*\n*—◉ ${usedPrefix + command} @${m.sender.split("@")[0]}*`
+    let why = `*JAWN STILL ERROR, EXAMPLE:*\n*—◉ ${usedPrefix + command} @${m.sender.split("@")[0]}*`
     let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
     if (!who) conn.reply(m.chat, why, m, { mentions: [m.sender] })
     let res = [];
