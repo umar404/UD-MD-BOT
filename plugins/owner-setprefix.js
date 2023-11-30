a72689ab78420c6bfd04ce5c8db41589474a6b07
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-11-30 08:57:29 -0800

    Update owner-setprefix.js

--- plugins/owner-setprefix.js
+++ plugins/owner-setprefix.js
@@ -1,16 +1,16 @@
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `No symbol detected ...`
+  if (!text) throw `Jawn Im Sorry No Symbol Detected ...`
 
   // Regular expression to check if the input contains exactly one symbol
   const symbolRegex = /^[^\w\s]{1}$/
 
   if (!symbolRegex.test(text)) {
-    throw `Invalid symbol input. Please provide exactly one symbol as a prefix.`
+    throw `🙂 Jawn Invalid symbol input. Please provide exactly one symbol as a prefix.`
   }
 
   // If the input is valid (contains exactly one symbol), update the prefix
   global.prefix = new RegExp('^[' + text.replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-  await m.reply(`the prefix has been changed to *${text}*`)
+  await m.reply(`Assalamualilkum Jawn UD MD BOT Prefix Has Been Changed To *${text}*`)
 }
 handler.help = ['.setprefix ', '.setprefix [symbol]']
 handler.tags = ['owner']
