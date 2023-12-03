0356aaffa74871d59c659a65d4ae2182c97f5bac
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-12-03 05:39:58 -0800

    Update nsfw-list.js

--- plugins/nsfw-list.js
+++ plugins/nsfw-list.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, usedPrefix }) => {
-    if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
+    if (!global.db.data.chats[m.chat].nsfw) throw `😾 Hey Jawn Group Doesnt SuppOrt Nsfw \n\n Enable it by \n*${usedPrefix}enable* nsfw`
     let user = global.db.data.users[m.sender].age
-    if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`) 
+    if (user < 17) throw m.reply(`🙂 Jawn Your Lolli Is So Small So You Need To Be Atleast 18 Years`) 
    
 m.react(rwait)
     let nsfwCommands = ['xnxx', 'xvid',  'genshin', 'swimsuit', 'schoolswimsuit', 'white', 'barefoot', 'touhou', 'gamecg', 'hololive', 'uncensored', 'sunglasses', 'glasses', 'weapon', 'shirtlift', 'chain', 'fingering', 'flatchest', 'torncloth', 'bondage', 'demon', 'wet', 'pantypull', 'headdress', 'headphone', 'tie', 'anusview', 'shorts', 'stockings', 'topless', 'beach', 'bunnygirl', 'bunnyear', 'idol', 'vampire', 'gun', 'maid', 'bra', 'nobra', 'bikini', 'whitehair', 'blonde', 'pinkhair', 'bed', 'ponytail', 'nude', 'dress', 'underwear', 'foxgirl', 'uniform', 'skirt', 'sex', 'sex2', 'sex3', 'breast', 'twintail', 'spreadpussy', 'tears', 'seethrough', 'breasthold', 'drunk', 'fateseries', 'spreadlegs', 'openshirt', 'headband', 'food', 'close', 'tree', 'nipples', 'erectnipples', 'horns', 'greenhair', 'wolfgirl', 'catgirl'];
   
     let message = `
-  *NSFW Commands Menu* 
+  *NSFW Commands Menu By UD-MD-BOT* 
   
-  Here is the list of available commands for NSFW content. Use the prefix "${usedPrefix}" followed by the command name:
+ 😻 Jawn Here Is The List Of Available Commands For NSFW Content. Use The Prefix "${usedPrefix}" Followed By The UD Bot Command Name 😽:
   
   ${nsfwCommands.map(command => `${usedPrefix}${command}`).join('\n')}
   
-  Note: These commands are only for those who are 18+.
+  Note 🙂: Jawn These commands are only for those who are 18+ Not For Small Lolli Persion😹🍌.
   `;
   
     console.log("Sending message...");
