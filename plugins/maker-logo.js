a7e2f98fc03e47e5e5d06b9d8295565aeb835dc2
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-12-03 06:35:00 -0800

    Update maker-logo.js

--- plugins/maker-logo.js
+++ plugins/maker-logo.js
@@ -1,97 +1,97 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-	let tee = `✳️ Enter a short text\n\n📌 Example  : *${usedPrefix + command}* GURU`
-	let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* GURU *+* BOT`
+	let tee = `😽 Jawn Enter a short text\n\n🤡 Example  : *${usedPrefix + command}* UMAR`
+	let too = `🙂 Jawn Separate the text with a *+* \n\n🤡 Example : \n*${usedPrefix + command}* UMAR *+* BOT`
 	
 	m.react(rwait);
 	
 	let type = command.toLowerCase();
 	
 	switch (type) {
-	  case 'gfx1':
+	  case 'ud1':
 		if (!text) throw tee;
 		let chut = `https://api.caliph.biz.id/api/kaneki?nama=${encodeURIComponent(text)}&apikey=caliphkey`;
-		conn.sendFile(m.chat, chut, 'logo.png', `✅ Result`, m);
+		conn.sendFile(m.chat, chut, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 		m.react(done);
 		break;
 		
-	  case 'gfx2': 
+	  case 'ud2': 
 		if (!text) throw too;
 		if (!text.includes('+')) throw too;
 		let [a, b] = text.split('+');
 		let loda = `https://api.caliph.biz.id/api/girlneko?nama=${encodeURIComponent(a.trim())}&nama2=${encodeURIComponent(b.trim())}&apikey=caliphkey`;
-		conn.sendFile(m.chat, loda, 'logo.png', `✅ Result`, m);
+		conn.sendFile(m.chat, loda, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 		
 		m.react(done);
 		break;
 		
-	  case 'gfx3':
+	  case 'ud3':
 		if (!text) throw tee;
 		let cp = `https://api.caliph.biz.id/api/rem?nama=${encodeURIComponent(text.trim())}&apikey=caliphkey`;
-		conn.sendFile(m.chat, cp, 'logo.png', `✅ Result`, m);
+		conn.sendFile(m.chat, cp, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 		m.react(done);
 		break;
 		
-	  case 'gfx4': 
+	  case 'ud4': 
 		if (!text) throw tee;
 		let gandu = `https://api.caliph.biz.id/api/textpro/matrix?text=${encodeURIComponent(text)}&apikey=caliphkey`;
-		conn.sendFile(m.chat, gandu, 'logo.png', `✅ Result`, m);
+		conn.sendFile(m.chat, gandu, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 		m.react(done);
 		break;
 		case 'gfx5':
     if (!text) throw tee
     const apiUrll = `https://api.lolhuman.xyz/api/textprome/jokerlogo?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-    conn.sendFile(m.chat, apiUrll, 'logo.png', '✅ Result', m);
-    m.react('✅');
+    conn.sendFile(m.chat, apiUrll, 'logo.png', '😻 Result By UD-MD-BOT', m);
+    m.react('😻');
     break;
 
-	case 'gfx6': 
+	case 'ud6': 
 	if (!text) throw too
 	if (!text.includes('+')) throw too  
 	let [c, d] = text.split`+`   
 	const apiUrl = `https://api.lolhuman.xyz/api/textprome2/lionlogo?apikey=${lolkeysapi}&text1=${encodeURIComponent(c)}&text2=${encodeURIComponent(d)}`
-	conn.sendFile(m.chat, apiUrl, 'logo.png', `✅ Result`, m)
+	conn.sendFile(m.chat, apiUrl, 'logo.png', `😻 Result By UD-MD-BOT`, m)
 	m.react(done)
 	break 
-	case 'gfx7': 
+	case 'ud7': 
 	if (!text) throw too;
 	if (!text.includes('+')) throw too;
 	let [e, f] = text.split('+');
 	let oda = `https://api.lolhuman.xyz/api/photooxy2/battlefield4?apikey=${lolkeysapi}&text1=${encodeURIComponent(e.trim())}&text2=${encodeURIComponent(f.trim())}`;
-	conn.sendFile(m.chat, oda, 'logo.png', `✅ Result`, m);
+	conn.sendFile(m.chat, oda, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 	
 	m.react(done);
 	break 
-	case 'gfx8': 
+	case 'ud8': 
 	if (!text) throw tee;
 	let rand = `https://api.lolhuman.xyz/api/ephoto1/anonymhacker?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, rand, 'logo.png', `✅ Result`, m);
+	conn.sendFile(m.chat, rand, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 	m.react(done);
 	break;
-	case 'gfx9': 
+	case 'ud9': 
 	if (!text) throw tee;
 	let randi = `https://api.lolhuman.xyz/api/ephoto1/avatarlolnew?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, randi, 'logo.png', `✅ Result`, m);
+	conn.sendFile(m.chat, randi, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 	m.react(done);
 	break;
-	case 'gfx10': 
+	case 'ud10': 
 	if (!text) throw tee;
 	let randu = `https://api.lolhuman.xyz/api/ephoto1/avatardota?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, randu, 'logo.png', `✅ Result`, m);
+	conn.sendFile(m.chat, randu, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 	m.react(done);
 	break;
-	case 'gfx11': 
+	case 'ud11': 
 	if (!text) throw too;
 	if (!text.includes('+')) throw too;
 	let [g, h] = text.split('+');
 	let od = `https://api.lolhuman.xyz/api/ephoto2/codwarzone?apikey=${lolkeysapi}&text1=${encodeURIComponent(g.trim())}&text2=${encodeURIComponent(h.trim())}`;
-	conn.sendFile(m.chat, od, 'logo.png', `✅ Result`, m);
+	conn.sendFile(m.chat, od, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 	
 	m.react(done);
 	break 
-	case 'gfx12': 
+	case 'ud12': 
 	if (!text) throw tee;
 	let rr = `https://api.lolhuman.xyz/api/ephoto1/freefire?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, rr, 'logo.png', `✅ Result`, m);
+	conn.sendFile(m.chat, rr, 'logo.png', `😻 Result By UD-MD-BOT`, m);
 	m.react(done);
 	break;
 
@@ -99,9 +99,9 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	} 
   };
   
-  handler.help = ['gfx1', 'gfx2', 'gfx3', 'gfx4', 'gfx5', 'gfx6', 'gfx7', 'gfx8', 'gfx9', 'gfx10', 'gfx11', 'gfx12'];
-  handler.tags = ['maker'];
-  handler.command = /^(gfx1|gfx2|gfx3|gfx4|gfx5|gfx6|gfx7|gfx8|gfx9|gfx10|gfx11|gfx12)$/i;
+  handler.help = ['ud1', 'ud2', 'ud3', 'ud4', 'ud5', 'ud6', 'ud7', 'ud8', 'ud9', 'ud10', 'ud11', 'ud12'];
+  handler.tags = ['ud maker'];
+  handler.command = /^(ud1|ud2|ud3|ud4|ud5|ud6|ud7|ud8|ud9|ud10|ud11|ud12)$/i;
   handler.diamond = false;
   
   export default handler;
