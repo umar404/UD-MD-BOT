aadba761aad87f2fa2ca966a0c76f71a8645108c
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-12-03 06:20:29 -0800

    Update maker-simp.js

--- plugins/maker-simp.js
+++ plugins/maker-simp.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/simpcard', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), 'error.png', '*your religion is simping*', m)
+    }), 'error.png', '*Jawn Your Religion Is Simping*', m)
     }
     handler.help = ['simpcard']
     handler.tags = ['maker']  
