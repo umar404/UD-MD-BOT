605ad04580e9bb7d7c7c6e3fa23098f3675ac7b2
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-12-04 05:08:49 -0800

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -1,13 +1,13 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 	
-let kickte = `✳️ Correct use of the command\n*${usedPrefix + command}* @tag`
+let kickte = `🥵 Jawn Correct use of the command\n*${usedPrefix + command}* @tag`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ User kicked`) 
+m.reply(`😽Phucking Bitch kicked by UD-MD-BOT`) 
 
 }
 
