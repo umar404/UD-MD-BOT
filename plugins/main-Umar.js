444d39cdcaea98fb94b3ae182fdbf021e14c5187
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-12-04 03:34:27 -0800

    Update and rename main-Guru.js to main-Umar.js

---
+++ plugins/main-Umar.js
@@ -0,0 +1,31 @@
+let handler = async m => m.reply(`
+
+≡  *UMAR BOT INFO
+
+─────────────
+😹 Jawn Join UD-MD-BOT Bot Support Channel.
+https://whatsapp.com/channel/0029VaEphvR3LdQbvGs4F50Z
+
+😻 UD MD BOT PLUGINS CHANNEL 😽
+https://whatsapp.com/channel/0029VaFP72qDJ6GvikQ5vV0a
+
+
+─────────────
+≡ Disabled links? enter here! 
+
+🥵 Contact With Developer Umar. 
+ https://wa.me/+923165123719
+─────────────
+👻 *UD Owner Telegram*
+ https://t.me/darkdevil719
+
+😽 *YouTube*
+• https://www.youtube.com/@DarkDevil404
+
+
+`.trim())
+handler.help = ['gpumar']
+handler.tags = ['main']
+handler.command = ['groups', 'umargc', 'udgc', 'ggp', 'udgroup'] 
+
+export default handler
