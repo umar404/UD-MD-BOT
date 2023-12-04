f87868caaea8335a5bc6d04139601d3f516ede2b
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-12-04 03:44:50 -0800

    Update img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -1,10 +1,10 @@
 import { pinterest } from '@bochilteam/scraper'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ What image do you want me to look for?\n\n📌 Example  : ${usedPrefix + command} Lil Peep`
+  if (!text) throw `🙂 Jawn WhatType Of image do you want me to look for?\n\n📌 Example  : ${usedPrefix + command} Aesthetic Umar`
   const json = await pinterest(text)
   conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
-*▢  Pinterest:*  ${text}
+*😽UD Pinterest:*  ${text}
 `.trim(), m)
 }
 handler.help = ['pinterest']
